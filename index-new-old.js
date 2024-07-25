39b0e37c0fd46af676d02e44f97488e27aa7f6d5
MaximilianoTRZ <maximilianoriverozuin@gmail.com>
2024-07-25 00:55:00 -0300

    latest changes in the index.js file

--- index-new-old.js
+++ index-new-old.js
@@ -1,61 +1,57 @@
-const qrcode = require('qrcode-terminal');
+const qrcode = require("qrcode-terminal");
 
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require("whatsapp-web.js");
 const client = new Client({
-  authStrategy: new LocalAuth()
+  authStrategy: new LocalAuth(),
 });
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
+client.on("ready", () => {
+  console.log("Client is ready!");
 });
 
-client.on('message', message => {
-	console.log(message.body);
+client.on("message", (message) => {
+  console.log(message.body);
 });
 
 // Mention everyone
 // https://wwebjs.dev/guide/mentioning-contacts.html#getting-mentioned-contacts
-client.on('message', async (msg) => {
-  if(msg.body === '!estoy') {
-
-    if(message.body === '!estoy') {
-      console.log('¡Mensaje recibido!', message);
+client.on("message", async (msg) => {
+  if (msg.body === "!estoy") {
+    if (message.body === "!estoy") {
+      console.log("¡Mensaje recibido!", message);
     }
-
-
 
     // const allChatMessages = (await msg.getChat()).fetchMessages();
     // // console.log( await allChatMessages)
-    
 
     // for (const mesag in allChatMessages) {
     //   console.log(`${mesag._data.notifyName}: ${mesag.body}`)
     // }
 
-      // const chat = await msg.getChat();
-      
-      // let text = "";
-      // let mentions = [];
+    // const chat = await msg.getChat();
 
-      // for(let participant of chat.participants) {
-      //     const contact = await client.getContactById(participant.id._serialized);
-          
-      //     mentions.push(contact);
-      //     text += `@${participant.id.user} `;
-      // }
+    // let text = "";
+    // let mentions = [];
 
-      // await chat.sendMessage(text, { mentions });
+    // for(let participant of chat.participants) {
+    //     const contact = await client.getContactById(participant.id._serialized);
+
+    //     mentions.push(contact);
+    //     text += `@${participant.id.user} `;
+    // }
+
+    // await chat.sendMessage(text, { mentions });
   }
 });
 
 // client.on('message', async (msg) => {
 //   const allMessages = (await msg.getChat()).fetchMessages;
 //   const contact = await msg.getContact();
-  
+
 //   await chat.sendMessage(`Hello @${contact.id.user}`, {
 //       mentions: [contact]
 //   });
@@ -65,12 +61,10 @@ client.on('message', async (msg) => {
 // client.on('message', async (msg) => {
 //     const chat = await msg.getChat();
 //     const contact = await msg.getContact();
-    
+
 //     await chat.sendMessage(`Hello @${contact.id.user}`, {
 //         mentions: [contact]
 //     });
 // });
-
-
 
 client.initialize();
